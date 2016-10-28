1a6aca014b8c7f5df6925a8fea781d2aded1b9f4
Sergey Ubogov <sergeiubogov@mail.ru>
2016-10-29 00:15:59 +0500

    сделал всё, добавил комменты для сложных для понимания функций

--- robbery.js
+++ robbery.js
@@ -17,9 +17,9 @@ exports.isStar = false;
 
 var CURRENT_YEAR = ' 2016 ';
 var CURRENT_MONTH = '2 ';
+var WEEK_DAYS = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
 
 function getTimeRegardingBank(oldDate, timeZone) {
-    var WEEK_DAYS = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
     var dayInOldDate = oldDate.split(' ')[0];
     var newDate = new Date(Date.parse(CURRENT_MONTH + (WEEK_DAYS.indexOf(dayInOldDate) + 1) +
         CURRENT_YEAR + oldDate.split(' ')[1].split('+')[0] + ' GMT'));
@@ -28,7 +28,7 @@ function getTimeRegardingBank(oldDate, timeZone) {
     return newDate;
 }
 
-function crossingInsideOrRight(friendBusyTime, currentFreeTime, freeTimes) {
+function searchCrossingInsideOrRight(friendBusyTime, currentFreeTime, freeTimes) {
     if (friendBusyTime.from >= currentFreeTime.begin &&
         friendBusyTime.from <= currentFreeTime.end) {
         if (friendBusyTime.to >= currentFreeTime.end) {
@@ -42,9 +42,11 @@ function crossingInsideOrRight(friendBusyTime, currentFreeTime, freeTimes) {
             });
         }
     }
+
+    return currentFreeTime;
 }
 
-function crossingLeft(friendBusyTime, currentFreeTime) {
+function searchCrossingLeft(friendBusyTime, currentFreeTime) {
     if (friendBusyTime.from <= currentFreeTime.begin &&
         friendBusyTime.to >= currentFreeTime.begin) {
         if (friendBusyTime.to >= currentFreeTime.end) {
@@ -53,11 +55,27 @@ function crossingLeft(friendBusyTime, currentFreeTime) {
             currentFreeTime.begin = friendBusyTime.to;
         }
     }
+
+    return currentFreeTime;
 }
 
 function searchCrossing(friendBusyTime, currentFreeTime, freeTimes) {
-    crossingLeft(friendBusyTime, currentFreeTime);
-    crossingInsideOrRight(friendBusyTime, currentFreeTime, freeTimes);
+    /*
+    В этих функциях весь алгоритм: есть freeTimes - в начале это просто свободное время банка в ПН,
+    ВТ и СР, в currentFreeTime находится свободное время в ПН, ВТ или СР. friendBusyTime - это
+    время занятости какого-либо из друзей в какой либо день. Представим friendBusyTime как отрезок
+    [f1, f2], currentFreeTime как отрезок [c1, c2], тогда, если f1<=c1 и f2>=c1, то есть пересечение слева
+    у отрезка [c1, c2] или [c1, c2] содержится в [f1, f2]. Тогда [c1, c2] становится [f2, c2](левая граница
+    сдвинулась) или [c1, c1](свободного времени нет в этот день). Если же f1>=c1 и f1 <= c2 то:
+    1) если f2 >= c2 значит отрезок [c1, c2] имеет справа пересечение с отрезком [f1, f2] и тогда [c1, c2]
+        становится [c1, f1]
+    2) если f2<= c2 значит отрезок [f1, f2] содержится в [c1, c2] и тогда [c1, c2] разделяется на 2 отрезка -
+        [c1, f1] и [f2, c1] 
+    */
+    currentFreeTime = searchCrossingLeft(friendBusyTime, currentFreeTime);
+    currentFreeTime = searchCrossingInsideOrRight(friendBusyTime, currentFreeTime, freeTimes);
+
+    return currentFreeTime;
 }
 
 function freeTimeSearch(goodDays, copySchedule) {
@@ -70,11 +88,29 @@ function freeTimeSearch(goodDays, copySchedule) {
         }
         indexGoodDay++;
     }
+
+    return goodDays;
 }
 
 function getTimeZoneRegardingBank(scheduleFriend, workingHours) {
     //  всё, что во времени находится после плюса является временной зоной
     return workingHours.from.split('+')[1] - scheduleFriend.from.split('+')[1];
+}
+
+function getWorkingTimeBank(goodDays, workingHours) {
+    var COUNT_GOOD_DAYS = 3;
+    for (var i = 1; i <= COUNT_GOOD_DAYS; i++) {
+        var beginningBankWork = workingHours.from.split('+')[0];
+        var endingBankWork = workingHours.to.split('+')[0];
+        goodDays.push({
+            begin: new Date(Date.parse(CURRENT_MONTH + i + CURRENT_YEAR +
+                beginningBankWork + ' GMT')),
+            end: new Date(Date.parse(CURRENT_MONTH + i + CURRENT_YEAR +
+                endingBankWork + ' GMT'))
+        });
+    }
+
+    return goodDays;
 }
 
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
@@ -83,8 +119,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     var timeZoneFriend = 0;
     for (var friend in schedule) {
         if (schedule.hasOwnProperty(friend)) {
-            copySchedule = copySchedule
-                           .concat(schedule[friend]);
+            copySchedule = copySchedule.concat(schedule[friend]);
         }
     }
     copySchedule = copySchedule.map(function (scheduleFriend) {
@@ -96,16 +131,8 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
         };
     });
     var goodDays = [];
-    var COUNT_GOOD_DAYS = 3;
-    for (var i = 1; i <= COUNT_GOOD_DAYS; i++) {
-        goodDays.push({
-            begin: new Date(Date.parse(CURRENT_MONTH + i + CURRENT_YEAR +
-                workingHours.from.split('+')[0] + ' GMT')),
-            end: new Date(Date.parse(CURRENT_MONTH + i + CURRENT_YEAR +
-                workingHours.to.split('+')[0] + ' GMT'))
-        });
-    }
-    freeTimeSearch(goodDays, copySchedule);
+    goodDays = getWorkingTimeBank(goodDays, workingHours);
+    goodDays = freeTimeSearch(goodDays, copySchedule);
     goodDays.sort(function (a, b) {
         return a.begin - b.begin;
     });
@@ -151,10 +178,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
                 minutes = minutes < 10 ? '0' + minutes : minutes;
                 template = template.replace('%HH', hours);
                 template = template.replace('%MM', minutes);
-                var NUMBER_DAY_TO_NAME = {
-                    1: 'ПН', 2: 'ВТ', 3: 'СР'
-                };
-                template = template.replace('%DD', NUMBER_DAY_TO_NAME[timeRobbery.getUTCDay()]);
+                template = template.replace('%DD', WEEK_DAYS[timeRobbery.getUTCDay() - 1]);
 
                 return template;
             }
