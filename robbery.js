7b261376abf35c180eb163d0e7a7a1004e89fe2a
Sergey Ubogov <sergeiubogov@mail.ru>
2016-10-29 19:27:10 +0500

    Update robbery.js

--- robbery.js
+++ robbery.js
@@ -15,15 +15,15 @@ exports.isStar = false;
  * @returns {Object}
  */
 
-var CURRENT_YEAR = ' 2016 ';
-var CURRENT_MONTH = '2 ';
+var CURRENT_YEAR = 2016;
+var CURRENT_MONTH = 2;
 var WEEK_DAYS = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
 
 function getTimeRegardingBank(oldDate, timeZone) {
     var dayInOldDate = oldDate.split(' ')[0];
     var oldTimeZoneFriend = oldDate.split(' ')[1].split('+')[0];
-    var newDate = new Date(Date.parse(CURRENT_MONTH + (WEEK_DAYS.indexOf(dayInOldDate) + 1) +
-        CURRENT_YEAR + oldTimeZoneFriend + ' GMT'));
+    var newDate = new Date(Date.parse(CURRENT_MONTH + ' ' + (WEEK_DAYS.indexOf(dayInOldDate) + 1) +
+        ' ' + CURRENT_YEAR + ' ' + oldTimeZoneFriend + ' GMT'));
     newDate.setUTCHours(newDate.getUTCHours() + timeZone);
 
     return newDate;
@@ -42,18 +42,19 @@ function getCopyGoodDays(goodDays) {
 
 function searchCrossingInsideOrRight(friendBusyTime, indexGoodDay, goodDays) {
     var copyGoodDays = getCopyGoodDays(goodDays);
-    if (friendBusyTime.from >= copyGoodDays[indexGoodDay].begin &&
-        friendBusyTime.from <= copyGoodDays[indexGoodDay].end) {
-        if (friendBusyTime.to >= copyGoodDays[indexGoodDay].end) {
-            copyGoodDays[indexGoodDay].end = friendBusyTime.from;
-        } else {
-            var end = new Date(String(copyGoodDays[indexGoodDay].end));
-            copyGoodDays[indexGoodDay].end = friendBusyTime.from;
-            copyGoodDays.push({
-                begin: friendBusyTime.to,
-                end: end
-            });
-        }
+    if (friendBusyTime.from < copyGoodDays[indexGoodDay].begin ||
+        friendBusyTime.from > copyGoodDays[indexGoodDay].end) {
+        return copyGoodDays;
+    }
+    if (friendBusyTime.to >= copyGoodDays[indexGoodDay].end) {
+        copyGoodDays[indexGoodDay].end = friendBusyTime.from;
+    } else {
+        var end = new Date(String(copyGoodDays[indexGoodDay].end));
+        copyGoodDays[indexGoodDay].end = friendBusyTime.from;
+        copyGoodDays.push({
+            begin: friendBusyTime.to,
+            end: end
+        });
     }
 
     return copyGoodDays;
@@ -61,13 +62,14 @@ function searchCrossingInsideOrRight(friendBusyTime, indexGoodDay, goodDays) {
 
 function searchCrossingLeft(friendBusyTime, indexGoodDay, goodDays) {
     var copyGoodDays = getCopyGoodDays(goodDays);
-    if (friendBusyTime.from <= copyGoodDays[indexGoodDay].begin &&
-        friendBusyTime.to >= copyGoodDays[indexGoodDay].begin) {
-        if (friendBusyTime.to >= copyGoodDays[indexGoodDay].end) {
-            copyGoodDays[indexGoodDay].end = copyGoodDays[indexGoodDay].begin;
-        } else {
-            copyGoodDays[indexGoodDay].begin = friendBusyTime.to;
-        }
+    if (friendBusyTime.from > copyGoodDays[indexGoodDay].begin ||
+        friendBusyTime.to < copyGoodDays[indexGoodDay].begin) {
+        return copyGoodDays;
+    }
+    if (friendBusyTime.to >= copyGoodDays[indexGoodDay].end) {
+        copyGoodDays[indexGoodDay].end = copyGoodDays[indexGoodDay].begin;
+    } else {
+        copyGoodDays[indexGoodDay].begin = friendBusyTime.to;
     }
 
     return copyGoodDays;
@@ -104,9 +106,9 @@ function getWorkingTimeBank(goodDays, workingHours) {
         var beginningBankWork = workingHours.from.split('+')[0];
         var endingBankWork = workingHours.to.split('+')[0];
         goodDays.push({
-            begin: new Date(Date.parse(CURRENT_MONTH + i + CURRENT_YEAR +
+            begin: new Date(Date.parse(CURRENT_MONTH + ' ' + i + ' ' + CURRENT_YEAR + ' ' +
                 beginningBankWork + ' GMT')),
-            end: new Date(Date.parse(CURRENT_MONTH + i + CURRENT_YEAR +
+            end: new Date(Date.parse(CURRENT_MONTH + ' ' + i + ' ' + CURRENT_YEAR + ' ' +
                 endingBankWork + ' GMT'))
         });
     }
@@ -165,26 +167,25 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            if (this.exists()) {
-                var timeRobbery = false;
-                goodDays.forEach(function (goodTime) {
-                    if ((goodTime.end - goodTime.begin) / SECOND_AND_MILLISECOND >= duration &&
-                        duration > 0 && !timeRobbery) {
-                        timeRobbery = goodTime.begin;
-                    }
-                });
-                var hours = timeRobbery.getUTCHours();
-                var minutes = timeRobbery.getUTCMinutes();
-                hours = hours < 10 ? '0' + hours : hours;
-                minutes = minutes < 10 ? '0' + minutes : minutes;
-                template = template.replace('%HH', hours);
-                template = template.replace('%MM', minutes);
-                template = template.replace('%DD', WEEK_DAYS[timeRobbery.getUTCDay() - 1]);
-
-                return template;
+            if (!this.exists()) {
+                return '';
             }
+            var timeRobbery = false;
+            goodDays.forEach(function (goodTime) {
+                if ((goodTime.end - goodTime.begin) / SECOND_AND_MILLISECOND >= duration &&
+                    duration > 0 && !timeRobbery) {
+                    timeRobbery = goodTime.begin;
+                }
+            });
+            var hours = timeRobbery.getUTCHours();
+            var minutes = timeRobbery.getUTCMinutes();
+            hours = hours < 10 ? '0' + hours : hours;
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+            template = template.replace('%HH', hours);
+            template = template.replace('%MM', minutes);
+            template = template.replace('%DD', WEEK_DAYS[timeRobbery.getUTCDay() - 1]);
 
-            return '';
+            return template;
         },
 
         /**
